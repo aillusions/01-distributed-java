a115e0dd81fc565446fcc7aaa55300ac72f1d4f1
aillusions <aillusions@gmail.com>
2019-02-03 22:25:22 +0200

    Redis - data

--- redis-data/src/main/java/com/zalizniak/redis/RedisConfig.java
+++ redis-data/src/main/java/com/zalizniak/redis/RedisConfig.java
@@ -1,11 +1,19 @@
 package com.zalizniak.redis;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+
+import javax.annotation.Resource;
+import java.net.URL;
 
 @Slf4j
 @Configuration
@@ -14,27 +22,19 @@ public class RedisConfig {
     @Value("${com.zalizniak.dj-redis.redis.host}")
     private String redisHost;
 
+
+    // @Bean
+    // public JedisConnectionFactory redisConnectionFactory() {
+    //     RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, 6379);
+    //     return new JedisConnectionFactory(config);
+    // }
+
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-
-        LettuceConnectionFactory jedisConFactory = new LettuceConnectionFactory();
-        jedisConFactory.setHostName(redisHost);
-        jedisConFactory.setPort(6379);
-        return jedisConFactory;
-    }
-
-    //@Bean
-    //public RedisTemplate<String, String> redisTemplate() {
-    //    RedisTemplate<String, String> template = new RedisTemplate<>();
-    //    template.setConnectionFactory(redisConnectionFactory());
-    //    // template.setEnableTransactionSupport(true);
-    //    return template;
-    //}
-
-    @Bean
-    public StringRedisTemplate stringRedisTemplate() {
-        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate(redisConnectionFactory());
-        stringRedisTemplate.setEnableTransactionSupport(true);
-        return stringRedisTemplate;
+        // LettuceConnectionFactory jedisConFactory = new LettuceConnectionFactory();
+        // jedisConFactory.setHostName(redisHost);
+        // jedisConFactory.setPort(6379);
+        // return jedisConFactory;
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisHost, 6379));
     }
 }
