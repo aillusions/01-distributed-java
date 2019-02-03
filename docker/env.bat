201da49f6d3eb8a564d06181b14760cb1b18acd3
aillusions <aillusions@gmail.com>
2019-02-03 12:40:57 +0200

    Spring boot admin

--- docker/env.bat
+++ docker/env.bat
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker-compose down
-docker-compose up dj-postgres dj-pgadmin  dj-redis dj-redis-commander dj-zoo dj-kafka dj-zoo-nav-api dj-zoo-nav-web dj-kafka-manager  dj-amq dj-mysql dj-adminer
+docker-compose up dj-postgres dj-pgadmin  dj-redis dj-redis-commander dj-zoo dj-kafka dj-zoo-nav-api dj-zoo-nav-web dj-kafka-manager  dj-amq dj-mysql dj-adminer dj-boot-admin
