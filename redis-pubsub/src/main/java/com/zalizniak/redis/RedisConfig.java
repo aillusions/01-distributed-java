5c9e2b9d911c8c88ef395bf5377cbff201dfbc67
aillusions <aillusions@gmail.com>
2019-02-04 09:30:56 +0200

    RedisPubsub - wip

--- redis-pubsub/src/main/java/com/zalizniak/redis/RedisConfig.java
+++ redis-pubsub/src/main/java/com/zalizniak/redis/RedisConfig.java
@@ -5,7 +5,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Slf4j
 @Configuration
@@ -16,25 +22,51 @@ public class RedisConfig {
 
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-
-        LettuceConnectionFactory jedisConFactory = new LettuceConnectionFactory();
-        jedisConFactory.setHostName(redisHost);
-        jedisConFactory.setPort(6379);
-        return jedisConFactory;
+        LettuceConnectionFactory lettuceConFactory = new LettuceConnectionFactory();
+        lettuceConFactory.setHostName(redisHost);
+        lettuceConFactory.setPort(6379);
+        return lettuceConFactory;
     }
 
-    //@Bean
-    //public RedisTemplate<String, String> redisTemplate() {
-    //    RedisTemplate<String, String> template = new RedisTemplate<>();
-    //    template.setConnectionFactory(redisConnectionFactory());
-    //    // template.setEnableTransactionSupport(true);
-    //    return template;
-    //}
+    @Bean
+    RedisTemplate<String, Object> redisTemplate() {
+        final RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericToStringSerializer<Object>(Object.class));
+        template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
+        return template;
+    }
 
     @Bean
     public StringRedisTemplate stringRedisTemplate() {
         StringRedisTemplate stringRedisTemplate = new StringRedisTemplate(redisConnectionFactory());
         stringRedisTemplate.setEnableTransactionSupport(true);
         return stringRedisTemplate;
+    }
+
+    @Bean
+    public MessageListenerAdapter messageListener() {
+        return new MessageListenerAdapter(new RedisMessageListener());
+    }
+
+    @Bean
+    public RedisMessageListenerContainer redisContainer() {
+        final RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+
+        container.setConnectionFactory(redisConnectionFactory());
+        container.addMessageListener(messageListener(), topic());
+
+        return container;
+    }
+
+    @Bean
+    public RedisPublisher redisPublisher() {
+        return new RedisPublisher(redisTemplate(), topic());
+    }
+
+    @Bean
+    public ChannelTopic topic() {
+        return new ChannelTopic("pubsub:queue");
     }
 }
---
+++ redis-pubsub/src/main/java/com/zalizniak/redis/RedisPublisher.java
@@ -0,0 +1,26 @@
+package com.zalizniak.redis;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.scheduling.annotation.Scheduled;
+
+public class RedisPublisher {
+
+    private final RedisTemplate<String, Object> template;
+    private final ChannelTopic topic;
+    private final AtomicLong counter = new AtomicLong(0);
+
+    public RedisPublisher(final RedisTemplate<String, Object> template,
+                          final ChannelTopic topic) {
+        this.template = template;
+        this.topic = topic;
+    }
+
+    @Scheduled(fixedDelay = 1000)
+    public void publish() {
+        template.convertAndSend(topic.getTopic(),
+                "Message " + counter.incrementAndGet() + ", " + Thread.currentThread().getName());
+    }
+}
