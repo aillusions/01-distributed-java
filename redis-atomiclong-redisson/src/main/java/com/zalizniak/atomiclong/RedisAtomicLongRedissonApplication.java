1e61e674567ac4a64b75ad649b4e462a64d032b5
aillusions <aillusions@gmail.com>
2019-02-06 13:33:32 +0200

    redis-atomiclong-redisson - added

---
+++ redis-atomiclong-redisson/src/main/java/com/zalizniak/atomiclong/RedisAtomicLongRedissonApplication.java
@@ -0,0 +1,14 @@
+package com.zalizniak.atomiclong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisAtomicLongRedissonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisAtomicLongRedissonApplication.class, args);
+	}
+
+}
+
