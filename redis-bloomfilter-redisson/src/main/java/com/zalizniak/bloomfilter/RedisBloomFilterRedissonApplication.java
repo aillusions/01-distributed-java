76aaf31e70b78627de8b7f70289980d9041a0e7e
aillusions <aillusions@gmail.com>
2019-02-06 12:48:31 +0200

    redis-bloomfilter-redisson added

---
+++ redis-bloomfilter-redisson/src/main/java/com/zalizniak/bloomfilter/RedisBloomFilterRedissonApplication.java
@@ -0,0 +1,14 @@
+package com.zalizniak.bloomfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisBloomFilterRedissonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisBloomFilterRedissonApplication.class, args);
+	}
+
+}
+
