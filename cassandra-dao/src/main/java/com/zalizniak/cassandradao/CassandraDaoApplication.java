2d5f8fbbdd8babecabed96429b4d310f10236116
alex <alex@platin.io>
2019-02-09 07:23:44 +0200

    Cassandra - wip

--- cassandra-dao/src/main/java/com/zalizniak/cassandradao/CassandraDaoApplication.java
+++ cassandra-dao/src/main/java/com/zalizniak/cassandradao/CassandraDaoApplication.java
@@ -1,17 +1,57 @@
 package com.zalizniak.cassandradao;
 
+import com.datastax.driver.core.utils.UUIDs;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * docker exec -it dj-cassandra-srv cqlsh
+ *
+ * CREATE KEYSPACE mykeyspace WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
+ *
+ * CREATE TABLE mykeyspace.customer (id TimeUUID PRIMARY KEY, firstname text, lastname text);
+ * CREATE INDEX customerfistnameindex ON mykeyspace.customer (firstname);
+ * CREATE INDEX customersecondnameindex ON mykeyspace.customer (lastname);
+ *
+ */
 @Slf4j
 @SpringBootApplication
 public class CassandraDaoApplication implements CommandLineRunner {
 
+    @Autowired
+    private CustomerRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
+
+        this.repository.deleteAll();
+
+        // save a couple of customers
+        this.repository.save(new Customer(UUIDs.timeBased(), "Alice", "Smith"));
+        this.repository.save(new Customer(UUIDs.timeBased(), "Bob", "Smith"));
+
+        // fetch all customers
+        log.info("Customers found with findAll():");
+        log.info("-------------------------------");
+        for (Customer customer : this.repository.findAll()) {
+            log.info("customer:" + customer);
+        }
+        log.info("");
+
+        // fetch an individual customer
+        log.info("Customer found with findByFirstName('Alice'):");
+        log.info("--------------------------------");
+        log.info("findByFirstName: " + this.repository.findByFirstName("Alice"));
+
+        log.info("Customers found with findByLastName('Smith'):");
+        log.info("--------------------------------");
+        for (Customer customer : this.repository.findByLastName("Smith")) {
+            log.info("customer:" + customer);
+        }
+
         log.info("CommandLineRunner executed.");
     }
 
---
+++ cassandra-dao/src/main/java/com/zalizniak/cassandradao/Customer.java
@@ -0,0 +1,22 @@
+package com.zalizniak.cassandradao;
+
+
+import lombok.*;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+import java.util.UUID;
+
+@ToString
+@Getter
+@Setter
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @PrimaryKey
+    private UUID id;
+    private String firstName;
+    private String lastName;
+}
