d64bd9e3c0e6f7baf3a6fe98351635852588961f
alex <alex@platin.io>
2019-02-09 07:09:25 +0200

    Cassandra - wip

--- cassandra-dao/src/main/java/com/zalizniak/cassandradao/CassandraDaoApplication.java
+++ cassandra-dao/src/main/java/com/zalizniak/cassandradao/CassandraDaoApplication.java
@@ -1,14 +1,23 @@
 package com.zalizniak.cassandradao;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class CassandraDaoApplication {
+public class CassandraDaoApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CassandraDaoApplication.class, args);
-	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("CommandLineRunner executed.");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CassandraDaoApplication.class, args);
+    }
 
 }
 
