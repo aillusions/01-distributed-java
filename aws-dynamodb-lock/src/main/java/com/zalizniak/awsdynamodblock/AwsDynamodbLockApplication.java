5402179d9b9cbb58397ccc807226231e6ed98b71
alex <alex@platin.io>
2019-02-10 20:06:43 +0200

    aws-dynamodb-lock - wip

---
+++ aws-dynamodb-lock/src/main/java/com/zalizniak/awsdynamodblock/AwsDynamodbLockApplication.java
@@ -0,0 +1,14 @@
+package com.zalizniak.awsdynamodblock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDynamodbLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDynamodbLockApplication.class, args);
+	}
+
+}
+
