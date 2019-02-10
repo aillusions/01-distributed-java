9c48fb80221993903119dd6c3f0473d48f84043e
alex <alex@platin.io>
2019-02-10 19:47:38 +0200

    AWS - Dynamodb - local - wip

--- aws-dynamodb-dao/src/main/java/com/zalizniak/awsdynamodbdao/AmazonDynamoDBConfig.java
+++ aws-dynamodb-dao/src/main/java/com/zalizniak/awsdynamodbdao/AmazonDynamoDBConfig.java
@@ -1,18 +1,33 @@
 package com.zalizniak.awsdynamodbdao;
 
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 @EnableDynamoDBRepositories(basePackageClasses = UserRepository.class)
 public class AmazonDynamoDBConfig {
 
+    @Value("${amazon.dynamodb.endpoint}")
+    private String amazonDynamoDBEndpoint;
+
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
-        return AmazonDynamoDBClientBuilder.standard().build();
+        AmazonDynamoDBClientBuilder builder = AmazonDynamoDBClientBuilder.standard();
+        if (StringUtils.isNotBlank(amazonDynamoDBEndpoint)) {
+            log.info("amazonDynamoDB created for: " + amazonDynamoDBEndpoint + " endpoint");
+            builder.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(amazonDynamoDBEndpoint, null));
+        } else {
+            log.info("amazonDynamoDB created for: default endpoint");
+        }
+        return builder.build();
     }
 
 }
