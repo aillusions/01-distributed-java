26e0dc8e824c716657d93161c674823c686a6c17
aillusions <aillusions@gmail.com>
2019-02-17 12:51:27 +0200

    Dynamodb - sdk - wip

---
+++ aws-dynamodb-sdk/src/main/java/com/zalizniak/awsdynamodbsdk/AwsDynamodbSdkApplication.java
@@ -0,0 +1,13 @@
+package com.zalizniak.awsdynamodbsdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDynamodbSdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDynamodbSdkApplication.class, args);
+	}
+
+}
---
+++ aws-dynamodb-sdk/src/main/java/com/zalizniak/awsdynamodbsdk/DynamoDbConf.java
@@ -0,0 +1,32 @@
+package com.zalizniak.awsdynamodbsdk;
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@Configuration
+public class DynamoDbConf {
+
+    @Bean()
+    @Resource(name = "amazonDynamoDB")
+    public DynamoDB dynamoDB(AmazonDynamoDB amazonDynamoDB) {
+        DynamoDB dynamoDB = new DynamoDB(amazonDynamoDB);
+        return dynamoDB;
+    }
+
+    @Bean
+    public AmazonDynamoDB amazonDynamoDB() {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "eu-west-1"))
+                .build();
+
+        return client;
+    }
+}
