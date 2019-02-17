dab6c5d521b2861936b07c0ca9df1db7ff32121f
aillusions <aillusions@gmail.com>
2019-02-17 20:06:33 +0200

    Zookeeper - config and discovery

--- zookeeper/src/main/java/com/zalizniak/zookeeper/ZookeeperApplication.java
+++ zookeeper/src/main/java/com/zalizniak/zookeeper/ZookeeperApplication.java
@@ -1,18 +1,61 @@
 package com.zalizniak.zookeeper;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.endpoint.event.RefreshEvent;
+import org.springframework.cloud.zookeeper.serviceregistry.ZookeeperServiceRegistry;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+import java.util.List;
+
+@Slf4j
+@EnableScheduling
 @Configuration
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ZookeeperApplication {
 
+    // Create: /configuration/DJ-Zookeeper/custom-dj-config-property
+    @Value("${custom-dj-config-property}")
+    private String property;
+
     public static void main(String[] args) {
         SpringApplication.run(ZookeeperApplication.class, args);
     }
 
+    @Autowired
+    private ZookeeperServiceRegistry serviceRegistry;
+
+    @Scheduled(fixedDelay = 5_000)
+    public void run() {
+        log.info("property: " + property);
+
+        List<ServiceInstance> list = discoveryClient.getInstances("DJ-Zookeeper");
+        list.forEach(s -> log.info("Service instance: " + s.toString()));
+    }
+
+    /*@Bean
+    CommandLineRunner commandLineRunner() {
+        return args -> {
+
+        };
+    }*/
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @EventListener(RefreshEvent.class)
+    public void onRefreshEvent(RefreshEvent evt) {
+        log.info("RefreshEvent: " + evt);
+    }
 }
 
--- zookeeper/src/test/java/com/zalizniak/zookeeper/ZookeeperApplicationTests.java
+++ zookeeper/src/test/java/com/zalizniak/zookeeper/ZookeeperApplicationTests.java
@@ -1,17 +1,25 @@
 package com.zalizniak.zookeeper;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ZookeeperApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Value("${custom-dj-config-property}")
+    private String property;
 
+    @Test
+    public void contextLoads() {
+        log.info("custom-dj-config-property: " + property);
+        Assert.assertEquals("Hello World", property);
+    }
 }
 
