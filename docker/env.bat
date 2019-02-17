6a1f9d552d96fe08c7a55ecbc7a2d979c9bf0f6d
aillusions <aillusions@gmail.com>
2019-02-17 14:20:34 +0200

    Zookepeer - wip

--- docker/env.bat
+++ docker/env.bat
@@ -2,4 +2,5 @@ docker-compose down
 docker-compose build
 @REM docker-compose up dj-nginx dj-postgres dj-pgadmin dj-redis dj-redis-commander dj-zoo dj-kafka dj-zoo-nav-api dj-zoo-nav-web dj-kafka-manager dj-amq
 @REM docker-compose up dj-datastax-dse dj-datastax-studio dj-h2o-3-ai
-docker-compose up dj-dynamodb
+@REM docker-compose up dj-dynamodb
+docker-compose up dj-zoo dj-zoo-nav-web dj-zoo-nav-api
--- zookeeper/src/main/java/com/zalizniak/zookeeper/ZookeeperApplication.java
+++ zookeeper/src/main/java/com/zalizniak/zookeeper/ZookeeperApplication.java
@@ -2,13 +2,17 @@ package com.zalizniak.zookeeper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ZookeeperApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZookeeperApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZookeeperApplication.class, args);
+    }
 
 }
 
