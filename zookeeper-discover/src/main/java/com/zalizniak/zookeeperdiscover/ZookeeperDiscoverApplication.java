937c3d4bf2ddc00ed24b1f41c1122b32b6402d8e
aillusions <aillusions@gmail.com>
2019-02-18 13:02:50 +0200

    Zookeeper - wip

---
+++ zookeeper-discover/src/main/java/com/zalizniak/zookeeperdiscover/ZookeeperDiscoverApplication.java
@@ -0,0 +1,13 @@
+package com.zalizniak.zookeeperdiscover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZookeeperDiscoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZookeeperDiscoverApplication.class, args);
+	}
+
+}
