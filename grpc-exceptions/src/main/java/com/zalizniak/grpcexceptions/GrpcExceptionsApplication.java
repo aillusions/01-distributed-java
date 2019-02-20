1af8d1fa507c73b1245e302466c519261377e49b
aillusions <aillusions@gmail.com>
2019-02-20 20:14:26 +0200

    GRPC - wip

---
+++ grpc-exceptions/src/main/java/com/zalizniak/grpcexceptions/GrpcExceptionsApplication.java
@@ -0,0 +1,13 @@
+package com.zalizniak.grpcexceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcExceptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcExceptionsApplication.class, args);
+	}
+
+}
