45f1b25057d244eca5ca7eedefd9743d067e639a
aillusions <aillusions@gmail.com>
2019-02-22 20:10:43 +0200

    GRPC - grpc-node js - wip

---
+++ grpc-nodejs/js/README.md
@@ -0,0 +1,4 @@
+
+npm install protobufjs [--save --save-prefix=~]
+
+pbjs -t static-module -w commonjs -o model.js src/main/proto/HelloService.proto
---
+++ grpc-nodejs/src/main/java/com/zaliznaik/grpccommunication/GrpcCommunicationApplication.java
@@ -0,0 +1,13 @@
+package com.zaliznaik.grpccommunication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcCommunicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcCommunicationApplication.class, args);
+    }
+
+}
