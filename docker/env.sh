45c119feb58c4e13b52e82c6c1bb25f2c7e13738
alex <alex@platin.io>
2019-02-27 06:01:45 +0200

    minor

--- docker/env.sh
+++ docker/env.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 docker-compose down
+docker-compose pull
+docker-compose up dj-datastax-dse dj-datastax-studio
+
 #docker-compose up dj-nginx dj-postgres dj-pgadmin dj-redis dj-redis-commander dj-zoo dj-kafka dj-zoo-nav-api dj-zoo-nav-web dj-kafka-manager dj-amq
 #docker-compose up dj-cassandra
-
 #docker-compose up dj-dynamodb
 #docker-compose up dj-spark-master dj-spark-worker-1
-
-docker-compose up dj-zoo-exhibitor dj-zoo-nav-web dj-zoo-nav-api
-
+#docker-compose up dj-zoo-exhibitor dj-zoo-nav-web dj-zoo-nav-api
