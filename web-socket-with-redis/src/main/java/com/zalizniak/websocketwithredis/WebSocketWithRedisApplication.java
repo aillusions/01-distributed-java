8b0cf0752408884eedc160e91dea5e3ea9c5ab35
aillusions <aillusions@gmail.com>
2019-02-28 07:02:04 +0200

    WebSocket - wip

---
+++ web-socket-with-redis/src/main/java/com/zalizniak/websocketwithredis/WebSocketWithRedisApplication.java
@@ -0,0 +1,13 @@
+package com.zalizniak.websocketwithredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketWithRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketWithRedisApplication.class, args);
+	}
+
+}
