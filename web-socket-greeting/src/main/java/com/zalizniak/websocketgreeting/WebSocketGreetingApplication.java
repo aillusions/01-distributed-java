353456c9aa9bb35d23983082e12bf187aef0745f
aillusions <aillusions@gmail.com>
2019-02-28 07:33:26 +0200

    WebSocket

---
+++ web-socket-greeting/src/main/java/com/zalizniak/websocketgreeting/WebSocketGreetingApplication.java
@@ -0,0 +1,13 @@
+package com.zalizniak.websocketgreeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketGreetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketGreetingApplication.class, args);
+	}
+
+}
