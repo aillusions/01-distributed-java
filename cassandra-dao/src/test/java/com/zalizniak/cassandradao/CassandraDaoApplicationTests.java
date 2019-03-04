7f605f5ba964a7d1d45e697a08177c6f599b10ea
aillusions <aillusions@gmail.com>
2019-03-04 07:52:50 +0200

    Cassandra wip

--- cassandra-dao/src/test/java/com/zalizniak/cassandradao/CassandraDaoApplicationTests.java
+++ cassandra-dao/src/test/java/com/zalizniak/cassandradao/CassandraDaoApplicationTests.java
@@ -1,16 +1,49 @@
 package com.zalizniak.cassandradao;
 
+import com.datastax.driver.core.utils.UUIDs;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CassandraDaoApplicationTests {
 
+	@Autowired
+	private CustomerRepository repository;
+
 	@Test
 	public void contextLoads() {
+		this.repository.deleteAll();
+
+		// save a couple of customers
+		this.repository.save(new Customer(UUIDs.timeBased(), "Alice", "Smith"));
+		this.repository.save(new Customer(UUIDs.timeBased(), "Bob", "Smith"));
+
+		// fetch all customers
+		log.info("Customers found with findAll():");
+		log.info("-------------------------------");
+		for (Customer customer : this.repository.findAll()) {
+			log.info("customer:" + customer);
+		}
+		log.info("");
+
+		// fetch an individual customer
+		log.info("Customer found with findByFirstName('Alice'):");
+		log.info("--------------------------------");
+		log.info("findByFirstName: " + this.repository.findByFirstName("Alice"));
+
+		log.info("Customers found with findByLastName('Smith'):");
+		log.info("--------------------------------");
+		for (Customer customer : this.repository.findByLastName("Smith")) {
+			log.info("customer:" + customer);
+		}
+
+		log.info("CommandLineRunner executed.");
 	}
 
 }
