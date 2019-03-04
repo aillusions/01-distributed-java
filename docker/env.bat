caf233cac2b4ca83c20de709d7f8516d6fa6eb01
aillusions <aillusions@gmail.com>
2019-03-04 07:45:32 +0200

    Cassandra wip

--- docker/env.bat
+++ docker/env.bat
@@ -1,13 +1,11 @@
 docker-compose down
 docker-compose pull
-
-@REM docker-compose up dj-sonarqube
-
-docker-compose up dj-redis dj-redis-commander
-
 @REM docker-compose build
+
+@REM docker-compose up dj-redis dj-redis-commander
+@REM docker-compose up dj-sonarqube
 @REM docker-compose up dj-nginx dj-postgres dj-pgadmin dj-redis dj-redis-commander dj-zoo dj-kafka dj-zoo-nav-api dj-zoo-nav-web dj-kafka-manager dj-amq
-@REM docker-compose up dj-datastax-dse dj-datastax-studio dj-h2o-3-ai
+docker-compose up dj-datastax-dse dj-datastax-studio
 @REM docker-compose up dj-dynamodb
 @REM docker-compose up dj-zoo dj-zoo-nav-web dj-zoo-nav-api
 
