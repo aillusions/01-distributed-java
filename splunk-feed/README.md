c28037963e08fdf8d33196e3a3c04cc53fc4cc67
aillusions <aillusions@gmail.com>
2019-03-15 12:18:22 +0200

    splunk - wip

---
+++ splunk-feed/README.md
@@ -0,0 +1,8 @@
+
+
+https://docs.splunk.com/Documentation/Splunk/7.2.4/SearchTutorial/GetthetutorialdataintoSplunk
+
+    
+    http://localhost:8000/en-US/manager/search/adddatamethods/selectsource?input_mode=0
+    
+    tutorialdata.zip
---
+++ splunk-feed/src/main/java/com/zalizniak/splunkfeed/SplunkFeedApplication.java
@@ -0,0 +1,13 @@
+package com.zalizniak.splunkfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplunkFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplunkFeedApplication.class, args);
+	}
+
+}
