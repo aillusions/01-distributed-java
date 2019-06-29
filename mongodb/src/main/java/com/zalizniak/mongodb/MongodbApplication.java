efed08b8da31506722f76487e4842988faf3e484
alex <alex@platin.io>
2019-06-29 14:14:07 +0300

    mongo db - wip

--- mongodb/src/main/java/com/zalizniak/mongodb/MongodbApplication.java
+++ mongodb/src/main/java/com/zalizniak/mongodb/MongodbApplication.java
@@ -1,13 +1,33 @@
 package com.zalizniak.mongodb;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoDatabase;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+import java.util.function.Consumer;
+
 @SpringBootApplication
-public class MongodbApplication {
+public class MongodbApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MongodbApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        MongoCredential credential = MongoCredential.createCredential("admin", "local", "admin".toCharArray());
+
+        MongoClient mongoClient = new MongoClient(new ServerAddress("localhost", 27017), Collections.singletonList(credential));
+
+        MongoDatabase database = mongoClient.getDatabase("local");
+
+        database.createCollection("customers", null);
+        database.listCollectionNames().forEach((Consumer<? super String>) System.out::println);
+    }
 }
