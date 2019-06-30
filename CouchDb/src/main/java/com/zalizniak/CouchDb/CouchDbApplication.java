b6573d5a78b7a3852809b4d6c58329fc141dab41
alex <alex@platin.io>
2019-06-30 12:31:48 +0300

    Couch DB - added

---
+++ CouchDb/src/main/java/com/zalizniak/CouchDb/CouchDbApplication.java
@@ -0,0 +1,13 @@
+package com.zalizniak.CouchDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouchDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouchDbApplication.class, args);
+	}
+
+}
