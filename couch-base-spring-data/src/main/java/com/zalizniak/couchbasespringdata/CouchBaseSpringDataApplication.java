f41c7436cc3fb9623a46bd0b1588e89db743719f
alex <alex@platin.io>
2019-07-01 15:26:52 +0300

    Cleanup

---
+++ couch-base-spring-data/src/main/java/com/zalizniak/couchbasespringdata/CouchBaseSpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.zalizniak.couchbasespringdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouchBaseSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouchBaseSpringDataApplication.class, args);
+	}
+
+}
