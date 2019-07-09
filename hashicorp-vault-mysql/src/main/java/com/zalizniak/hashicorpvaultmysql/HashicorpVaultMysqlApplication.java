f3a3fc563fa066339e7d93ba60def718998de022
alex <alex@platin.io>
2019-07-09 05:11:00 +0300

    vault - mysql connect

---
+++ hashicorp-vault-mysql/src/main/java/com/zalizniak/hashicorpvaultmysql/HashicorpVaultMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.zalizniak.hashicorpvaultmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashicorpVaultMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashicorpVaultMysqlApplication.class, args);
+	}
+
+}
---
+++ hashicorp-vault-mysql/src/test/java/com/zalizniak/hashicorpvaultmysql/HashicorpVaultMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zalizniak.hashicorpvaultmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashicorpVaultMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
