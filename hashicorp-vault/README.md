e486b2868016596e013bd63ef523f6da28a14db4
alex <alex@platin.io>
2019-07-09 18:12:11 +0300

    vault - decrypt / encrypt

--- hashicorp-vault/README.md
+++ hashicorp-vault/README.md
@@ -1,11 +1,19 @@
 
 
 export VAULT_ADDR='http://127.0.0.1:8200'
+
+vault operator init
+
 vault login
     myroot
     
-vault kv put secret/my-application example.username=demouser example.password=demopassword
+vault token create
 
+vault secrets enable transit
+
+vault write -force transit/keys/my-key
+    
+vault kv put secret/my-application example.username=demouser example.password=demopassword
 
 Run HashicorpVaultApplication with -Dspring.cloud.vault.token=myroot
 or uncomment token: myroot in bootstrap.yml
--- hashicorp-vault/src/main/java/com/zalizniak/hashicorpvault/HashicorpVaultApplication.java
+++ hashicorp-vault/src/main/java/com/zalizniak/hashicorpvault/HashicorpVaultApplication.java
@@ -2,10 +2,15 @@ package com.zalizniak.hashicorpvault;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.vault.core.VaultOperations;
+import org.springframework.vault.core.VaultTransitOperations;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableConfigurationProperties(MyConfiguration.class)
@@ -21,6 +26,16 @@ public class HashicorpVaultApplication implements CommandLineRunner {
         this.configuration = configuration;
     }
 
+    @Autowired
+    private VaultOperations vaultOperations;
+
+    private VaultTransitOperations transitOperations;
+
+    @PostConstruct
+    public void postConstruct() {
+        transitOperations = vaultOperations.opsForTransit();
+    }
+
     @Override
     public void run(String... args) throws Exception {
         Logger logger = LoggerFactory.getLogger(HashicorpVaultApplication.class);
@@ -30,5 +45,17 @@ public class HashicorpVaultApplication implements CommandLineRunner {
         logger.info("        example.username is {}", configuration.getUsername());
         logger.info("        example.password is {}", configuration.getPassword());
         logger.info("----------------------------------------");
+
+        String ciphertext = transitOperations.encrypt("my-key",
+                "This is my secret string. Better not use umlauts here. "
+                        + "This string is converted to bytes using String.getBytes() so "
+                        + "make sure to set -Dfile.encoding=... properly.");
+
+        System.out.println("Encrypted: " + ciphertext);
+
+        String plaintext = transitOperations.decrypt("my-key", ciphertext);
+
+        System.out.println("plaintext: " + plaintext);
+
     }
 }
