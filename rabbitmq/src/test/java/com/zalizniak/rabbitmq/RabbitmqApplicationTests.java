bb821cb99754bca50a8bcf3dc75cc0ed35c9af08
alex <alex@zalizniak.com>
2019-09-18 13:43:45 +0300

    Rabbit MQ - wip

--- rabbitmq/src/test/java/com/zalizniak/rabbitmq/RabbitmqApplicationTests.java
+++ rabbitmq/src/test/java/com/zalizniak/rabbitmq/RabbitmqApplicationTests.java
@@ -1,9 +1,6 @@
 package com.zalizniak.rabbitmq;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.DeliverCallback;
+import com.rabbitmq.client.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,16 +9,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 /**
- * TODO see more https://github.com/rabbitmq/rabbitmq-tutorials/tree/master/java
+ * TODO see more
+ * https://www.rabbitmq.com/tutorials/tutorial-two-java.html
+ * https://github.com/rabbitmq/rabbitmq-tutorials/tree/master/java
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RabbitmqApplicationTests {
 
     private static final String QUEUE_NAME = "my_queue";
+    private static final String TASK_QUEUE_NAME = "my_task_queue";
 
     private static ConnectionFactory factory;
 
@@ -69,7 +70,64 @@ public class RabbitmqApplicationTests {
             channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
             });
 
-            doneSignal.await();
+            doneSignal.await(10, TimeUnit.SECONDS);
+        }
+    }
+
+    @Test
+    public void testWorkerSent() throws IOException, TimeoutException, InterruptedException {
+
+        try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
+
+            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
+            String message = "nice to meet u";
+
+            channel.basicPublish("", TASK_QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+
+    @Test
+    public void testWorker() throws IOException, TimeoutException, InterruptedException {
+
+        CountDownLatch doneSignal = new CountDownLatch(1);
+
+        try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
+
+            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+            System.out.println(" [*] Waiting for messages.");
+
+            channel.basicQos(1);
+
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String message = new String(delivery.getBody(), "UTF-8");
+
+                System.out.println(" [x] Received '" + message + "'");
+                try {
+                    doWork(message);
+                } finally {
+                    System.out.println(" [x] Done");
+                    channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                    doneSignal.countDown();
+                }
+            };
+            channel.basicConsume(TASK_QUEUE_NAME, false, deliverCallback, consumerTag -> {
+            });
+
+            doneSignal.await(10, TimeUnit.SECONDS);
+        }
+    }
+
+    private static void doWork(String task) {
+        for (char ch : task.toCharArray()) {
+            if (ch == '.') {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException _ignored) {
+                    Thread.currentThread().interrupt();
+                }
+            }
         }
     }
 }
