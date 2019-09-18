14434ddad09235de9a621fc717c78f28cbb5c707
alex <alex@zalizniak.com>
2019-09-18 12:34:38 +0300

    Rabbit MQ - wip

--- rabbitmq/src/test/java/com/zalizniak/rabbitmq/RabbitmqApplicationTests.java
+++ rabbitmq/src/test/java/com/zalizniak/rabbitmq/RabbitmqApplicationTests.java
@@ -3,6 +3,8 @@ package com.zalizniak.rabbitmq;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,21 +13,29 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
+/**
+ * TODO see more https://github.com/rabbitmq/rabbitmq-tutorials/tree/master/java
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RabbitmqApplicationTests {
 
     private static final String QUEUE_NAME = "my_queue";
 
-    @Test
-    public void contextLoads() {
+    private static ConnectionFactory factory;
 
-        ConnectionFactory factory = new ConnectionFactory();
+    @BeforeClass
+    public static void setClass() {
+        factory = new ConnectionFactory();
         factory.setHost("localhost");
         factory.setUsername("user");
         factory.setPassword("password");
-        try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel()) {
+    }
+
+    @Test
+    public void testSend() {
+
+        try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
 
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
             String message = "Hello World!";
@@ -39,4 +49,23 @@ public class RabbitmqApplicationTests {
         }
     }
 
+    @Test
+    public void testReceive() throws IOException, TimeoutException, InterruptedException {
+
+        try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
+
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            System.out.println(" [*] Waiting for messages.");
+
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String message = new String(delivery.getBody(), "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+            };
+
+            channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
+            });
+
+            Thread.sleep(1000);
+        }
+    }
 }
