7361e37edc8ddd8fccdd27c8fb9552aee1427112
alex <alex@platin.io>
2019-09-27 16:28:12 +0300

    hazelcast - wip

--- hazelcast/src/main/java/com/zalizniak/hazel/App.java
+++ hazelcast/src/main/java/com/zalizniak/hazel/App.java
@@ -1,10 +1,18 @@
 package com.zalizniak.hazel;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
+
+        Config cfg = new Config();
+        HazelcastInstance instance = Hazelcast.newHazelcastInstance(cfg);
+
         System.out.println("Hello World!");
     }
 }
