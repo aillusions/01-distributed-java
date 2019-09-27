17151c40a0a85d2508401321efee054d005c737c
alex <alex@platin.io>
2019-09-27 16:55:14 +0300

    hazelcast - wip

--- hazelcast/src/test/java/com/zalizniak/hazel/AppTest.java
+++ hazelcast/src/test/java/com/zalizniak/hazel/AppTest.java
@@ -3,9 +3,11 @@ package com.zalizniak.hazel;
 import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IExecutorService;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.Serializable;
 import java.util.Map;
 import java.util.Queue;
 
@@ -38,5 +40,17 @@ public class AppTest {
         Assert.assertEquals("Tom", queueCustomers.poll());
         Assert.assertEquals("Mary", queueCustomers.peek());
         Assert.assertEquals(2, queueCustomers.size());
+
+        IExecutorService exec = instance0.getExecutorService("Exec");
+        exec.submit(new MyDistributedTask());
+        exec.submit(new MyDistributedTask());
+    }
+
+    public static class MyDistributedTask implements Runnable, Serializable {
+
+        @Override
+        public void run() {
+            System.out.println("Hello from task... from " + Thread.currentThread().getName());
+        }
     }
 }
