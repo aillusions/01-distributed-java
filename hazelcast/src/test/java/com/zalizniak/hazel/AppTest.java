c96b6bcae1a5dcd31efff850c3fd3d2e35a60309
alex <alex@platin.io>
2019-09-28 04:53:26 +0300

    hazelcast jet - wip

--- hazelcast/src/test/java/com/zalizniak/hazel/AppTest.java
+++ hazelcast/src/test/java/com/zalizniak/hazel/AppTest.java
@@ -4,12 +4,22 @@ import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IExecutorService;
+import com.hazelcast.jet.Jet;
+import com.hazelcast.jet.JetInstance;
+import com.hazelcast.jet.pipeline.Pipeline;
+import com.hazelcast.jet.pipeline.Sinks;
+import com.hazelcast.jet.pipeline.Sources;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.Map;
 import java.util.Queue;
+
+import static com.hazelcast.jet.Traversers.traverseArray;
+import static com.hazelcast.jet.aggregate.AggregateOperations.counting;
+import static com.hazelcast.jet.function.Functions.wholeItem;
 
 /**
  *
@@ -52,5 +62,41 @@ public class AppTest {
         public void run() {
             System.out.println("Hello from task... from " + Thread.currentThread().getName());
         }
+    }
+
+    @Test
+    public void testJet() {
+
+        // Create the specification of the computation pipeline. Note
+        // it's a pure POJO: no instance of Jet needed to create it.
+        Pipeline p = Pipeline.create();
+        p.drawFrom(Sources.<String>list("text"))
+                .flatMap(word ->
+                        traverseArray(word.toLowerCase().split("\\W+")))
+                .filter(word -> !word.isEmpty())
+                .groupingKey(wholeItem())
+                .aggregate(counting())
+                .drainTo(Sinks.map("counts"));
+
+        // Start Jet, populate the input list
+        JetInstance jet = Jet.newJetInstance();
+        try {
+            List<String> text = jet.getList("text");
+            text.add("hello world hello hello world");
+            text.add("world world hello world");
+
+            // Perform the computation
+            jet.newJob(p).join();
+
+            // Check the results
+            Map<String, Long> counts = jet.getMap("counts");
+            System.out.println("Count of hello: "
+                    + counts.get("hello"));
+            System.out.println("Count of world: "
+                    + counts.get("world"));
+        } finally {
+            Jet.shutdownAll();
+        }
+
     }
 }
